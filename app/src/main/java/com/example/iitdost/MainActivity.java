17b469477d4455d375a95ea4ed137f4d9ed8119f
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2018-03-14 00:43:53 +0530

    Restructured into fragments and documented code

--- app/src/main/java/com/example/iitdost/MainActivity.java
+++ app/src/main/java/com/example/iitdost/MainActivity.java
@@ -5,12 +5,14 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -21,28 +23,9 @@ import com.aurelhubert.ahbottomnavigation.AHBottomNavigationAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private TextView mTextMessage;
     private DrawerLayout mDrawerLayout;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_requests:
-                    mTextMessage.setText(R.string.title_requests);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,16 +52,71 @@ public class MainActivity extends AppCompatActivity {
 
         mTextMessage = (TextView) findViewById(R.id.message);
 
+        //Initialize bottom navigation bar with different colors and menu items
         int[] tabColors = getApplicationContext().getResources().getIntArray(R.array.tab_colors);
         AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById(R.id.navigation);
         AHBottomNavigationAdapter navigationAdapter = new AHBottomNavigationAdapter(this, R.menu.navigation);
         navigationAdapter.setupWithBottomNavigation(bottomNavigation, tabColors);
+
+        //Setting Color for active icons on bottom navigation bar
         bottomNavigation.setAccentColor(Color.parseColor("#000000"));
+
+        //Setting Color for inactive icons on bottom navigation bar
         bottomNavigation.setInactiveColor(Color.parseColor("#c3baba"));
-        bottomNavigation.setTitleTextSize(45,30);
+
+        //Setting text size for active and inactive states
+        bottomNavigation.setTitleTextSize(45, 30);
+
+        //Setting typeface for titles on bottom navigation bar
         Typeface typeface = ResourcesCompat.getFont(getApplicationContext(), R.font.opensans_semibold);
         bottomNavigation.setTitleTypeface(typeface);
 
+        // Setting listeners for tab selection
+        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public boolean onTabSelected(int position, boolean wasSelected) {
+
+                //If previously selected then skip
+                if(wasSelected){
+                    return true;
+                }
+
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                switch (position) {
+                    case 0:
+                        Log.d(TAG, "onNavigationItemSelected: Navigating to Home");
+                        // Replace the contents of the container with the new fragment
+                        ft.replace(R.id.home_frag_space, new HomeFragment());
+                        // or ft.add(R.id.your_placeholder, new FooFragment());
+                        // Complete the changes added above
+                        ft.commit();
+//                    mTextMessage.setText(R.string.title_home);
+                        return true;
+                    case 1:
+                        // Replace the contents of the container with the new fragment
+                        ft.replace(R.id.home_frag_space, new NotificationFragment());
+                        // or ft.add(R.id.your_placeholder, new FooFragment());
+                        // Complete the changes added above
+                        ft.commit();
+//                    mTextMessage.setText(R.string.title_notifications);
+                        return true;
+                    case 2:
+                        // Replace the contents of the container with the new fragment
+                        ft.replace(R.id.home_frag_space, new RequestsFragment());
+                        // or ft.add(R.id.your_placeholder, new FooFragment());
+                        // Complete the changes added above
+                        ft.commit();
+//                    mTextMessage.setText(R.string.title_requests);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        //Initiating screen on home fragment
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.home_frag_space, new HomeFragment());
+        ft.commit();
     }
 
     @Override
